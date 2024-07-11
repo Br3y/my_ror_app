8fa6ccbd12fc9f6e04b23b3088c0cc06ec44f037
Aubrey <lizardoaubrey.software@gmail.com>
2024-07-11 16:52:51 +0800

    feat(routes): long way to write restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Rails.application.routes.draw do
   resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Long way to write RESTful routes
+  get "/posts", to: "posts#index"
+  get "/posts/new", to: "posts#new"
+  get "/posts/:id", to: "posts#show"
+  post "/posts", to: "posts#create"
+  get "/posts/:id/edit", to: "posts#edit"
+  put "/posts/:id", to: "posts#update"
+  delete "/posts/:id", to: "posts#destroy" 
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
